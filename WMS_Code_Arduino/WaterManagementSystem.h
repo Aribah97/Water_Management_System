abdb3c5e863b0fd18bf606818673f49e2adb9238
Aribah Ibrahim <152000741+Aribah97@users.noreply.github.com>
2026-03-03 11:56:17 +0100

    Create WaterManagementSystem.h with class definition

---
+++ WMS_Code_Arduino/WaterManagementSystem.h
@@ -0,0 +1,16 @@
+#ifndef WATER_MANAGEMENT_SYSTEM_H
+#define WATER_MANAGEMENT_SYSTEM_H
+
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+
+class WaterManagementSystem {
+public:
+    WaterManagementSystem();
+    void begin();
+    void loop();
+    // Additional methods and attributes
+};
+
+#endif // WATER_MANAGEMENT_SYSTEM_H
